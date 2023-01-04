e46a27ccfde0d5db5e8e36b79380ee7379fcb860
Lars Strojny <lars@strojny.net>
2023-01-04 23:13:13 +0100

    Switch to rust-overlay for nightly rust

--- flake.nix
+++ flake.nix
@@ -3,16 +3,18 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { system = system; };
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
         rust-toolchain = pkgs.symlinkJoin {
           name = "rust-toolchain";
-          paths = [ pkgs.rustc pkgs.cargo pkgs.rustfmt pkgs.rustPlatform.rustcSrc ];
+          paths = [ pkgs.rust-bin.beta.latest.default ];
         };
       in { devShell = pkgs.mkShell { packages = [ rust-toolchain ]; }; });
 }
