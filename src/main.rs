241de06f2b1622f00ee7cff203c473cc3f51b38c
Lars Strojny <lars@strojny.net>
2023-01-04 22:22:43 +0100

    Trivial webserver

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+extern crate rocket;
+
+use rocket::{get, launch, routes};
+
+#[get("/")]
+async fn index() -> String {
+    return "Hello, world!".to_string();
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
