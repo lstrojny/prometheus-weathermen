28adc51e09cbbf78c05783f676449552782fd687
Lars Strojny <lars@strojny.net>
2023-02-14 19:09:10 +0100

    Use atomic cache initialization

--- src/providers/http_request.rs
+++ src/providers/http_request.rs
@@ -98,121 +98,112 @@ fn serde_deserialize_body<T: Debug + DeserializeOwned>(body: &str) -> anyhow::Re
 
 pub fn request_cached<R: Debug>(request: &HttpCacheRequest<R>) -> anyhow::Result<R> {
     let key = (request.method.clone(), request.url.clone());
-    let value = request.cache.get(&key);
 
-    debug!(
-        "Checking cache item for request \"{:#} {:#}\" for {:?} with lifetime {:?}",
-        request.method,
-        request.url,
-        request.source,
-        request
-            .cache
-            .policy()
-            .time_to_live()
-            .unwrap_or(Duration::from_secs(0))
-    );
-
-    if let Some(value) = value {
+    let value = request.cache.try_get_with_by_ref(&key, || {
         debug!(
-            "Found cached item for \"{:#} {:#}\"",
-            request.method, request.url
+            "Generating cache item for request \"{:#} {:#}\" for {:?} with lifetime {:?}",
+            request.method,
+            request.url,
+            request.source,
+            request
+                .cache
+                .policy()
+                .time_to_live()
+                .unwrap_or(Duration::from_secs(0))
         );
 
-        let des = (request.deserialize)(&value)?;
+        let cicruit_breaker_scope = request
+            .url
+            .host_str()
+            .ok_or_else(|| anyhow!("Could not extract host from URL"))?;
 
-        return Ok(des);
-    }
+        // Separate scope so read lock is dropped at the end if circuit breaker does not yet exist
+        {
+            let circuit_breaker_registry_ro =
+                CIRCUIT_BREAKER_REGISTRY.read().expect("Poisoned lock");
 
-    debug!(
-        "No cache item found for \"{:#} {:#}\". Requesting",
-        request.method, request.url
-    );
+            trace!("Read lock acquired for {:?}", cicruit_breaker_scope);
 
-    let cicruit_breaker_scope = request
-        .url
-        .host_str()
-        .ok_or_else(|| anyhow!("Could not extract host from URL"))?;
+            if let Some(cb) = circuit_breaker_registry_ro.get(cicruit_breaker_scope) {
+                return request_url_with_circuit_breaker(cicruit_breaker_scope, cb, request);
+            }
 
-    // Separate scope so read lock is dropped at the end if circuit breaker does not yet exist
-    {
-        let circuit_breaker_registry_ro = CIRCUIT_BREAKER_REGISTRY.read().expect("Poisoned lock");
-
-        trace!("Read lock acquired for {:?}", cicruit_breaker_scope);
-
-        if let Some(cb) = circuit_breaker_registry_ro.get(cicruit_breaker_scope) {
-            return request_url_with_circuit_breaker(cicruit_breaker_scope, cb, request, &key);
+            drop(circuit_breaker_registry_ro);
         }
 
-        drop(circuit_breaker_registry_ro);
-    }
-
-    // Separate scope so write lock is dropped at the end
-    {
-        trace!(
-            "Trying to acquire write lock to instantiate circuit breaker {:?}",
-            cicruit_breaker_scope
-        );
-
-        let mut circuit_breaker_registry_rw =
-            CIRCUIT_BREAKER_REGISTRY.write().expect("Poisoned lock");
-        trace!(
-            "Write lock acquired to instantiate circuit breaker {:?}",
-            cicruit_breaker_scope
-        );
-
-        if circuit_breaker_registry_rw.contains_key(cicruit_breaker_scope) {
+        // Separate scope so write lock is dropped at the end
+        {
             trace!(
-                "Circuit breaker {:?} already instantiated, skipping",
-                cicruit_breaker_scope
-            );
-        } else {
-            trace!(
-                "Circuit breaker {:?} not yet instantiated, instantiating",
+                "Trying to acquire write lock to instantiate circuit breaker {:?}",
                 cicruit_breaker_scope
             );
 
-            let circuit_breaker = Config::new()
-                .failure_policy(consecutive_failures(
-                    CONSECUTIVE_FAILURE_COUNT,
-                    exponential(
-                        Duration::from_secs(EXPONENTIAL_BACKOFF_START_SECS),
-                        Duration::from_secs(EXPONENTIAL_BACKOFF_MAX_SECS),
-                    ),
-                ))
-                .build();
+            let mut circuit_breaker_registry_rw =
+                CIRCUIT_BREAKER_REGISTRY.write().expect("Poisoned lock");
+            trace!(
+                "Write lock acquired to instantiate circuit breaker {:?}",
+                cicruit_breaker_scope
+            );
 
-            circuit_breaker_registry_rw.insert(cicruit_breaker_scope.to_string(), circuit_breaker);
+            if circuit_breaker_registry_rw.contains_key(cicruit_breaker_scope) {
+                trace!(
+                    "Circuit breaker {:?} already instantiated, skipping",
+                    cicruit_breaker_scope
+                );
+            } else {
+                trace!(
+                    "Circuit breaker {:?} not yet instantiated, instantiating",
+                    cicruit_breaker_scope
+                );
 
-            trace!("Circuit breaker {:?} instantiated", cicruit_breaker_scope);
+                let circuit_breaker = Config::new()
+                    .failure_policy(consecutive_failures(
+                        CONSECUTIVE_FAILURE_COUNT,
+                        exponential(
+                            Duration::from_secs(EXPONENTIAL_BACKOFF_START_SECS),
+                            Duration::from_secs(EXPONENTIAL_BACKOFF_MAX_SECS),
+                        ),
+                    ))
+                    .build();
+
+                circuit_breaker_registry_rw
+                    .insert(cicruit_breaker_scope.to_string(), circuit_breaker);
+
+                trace!("Circuit breaker {:?} instantiated", cicruit_breaker_scope);
+            }
+
+            drop(circuit_breaker_registry_rw);
         }
 
-        drop(circuit_breaker_registry_rw);
+        trace!(
+            "Trying to acquire read lock after circuit breaker {:?} was instantiated",
+            cicruit_breaker_scope
+        );
+        let circuit_breaker_registry_ro = CIRCUIT_BREAKER_REGISTRY
+            .read()
+            .expect("Lock should not be poisoned");
+        trace!(
+            "Read lock acquired after circuit breaker {:?} was instantiated",
+            cicruit_breaker_scope
+        );
+        let circuit_breaker = circuit_breaker_registry_ro
+            .get(cicruit_breaker_scope)
+            .expect("Circuit breaker must now exist");
+
+        request_url_with_circuit_breaker(cicruit_breaker_scope, circuit_breaker, request)
+    });
+
+    match value {
+        Ok(v) => Ok((request.deserialize)(&v)?),
+        Err(e) => Err(anyhow!(e)),
     }
-
-    trace!(
-        "Trying to acquire read lock after circuit breaker {:?} was instantiated",
-        cicruit_breaker_scope
-    );
-    let circuit_breaker_registry_ro = CIRCUIT_BREAKER_REGISTRY
-        .read()
-        .expect("Lock should not be poisoned");
-    trace!(
-        "Read lock acquired after circuit breaker {:?} was instantiated",
-        cicruit_breaker_scope
-    );
-    let circuit_breaker = circuit_breaker_registry_ro
-        .get(cicruit_breaker_scope)
-        .expect("Circuit breaker must now exist");
-
-    request_url_with_circuit_breaker(cicruit_breaker_scope, circuit_breaker, request, &key)
 }
 
 fn request_url_with_circuit_breaker<R: Debug>(
     circuit_breaker_scope: &str,
     circuit_breaker: &HttpCircuitBreaker,
     request: &HttpCacheRequest<R>,
-    key: &(Method, Url),
-) -> anyhow::Result<R> {
+) -> anyhow::Result<String> {
     match circuit_breaker.call(|| request_url(request)) {
         Err(Error::Inner(e)) => Err(anyhow!(e)),
         Err(Error::Rejected) => Err(anyhow!(
@@ -226,13 +217,7 @@ fn request_url_with_circuit_breaker<R: Debug>(
                 response.status()
             );
 
-            let body = (request.to_string)(response)?;
-
-            request.cache.insert(key.clone(), body.clone());
-
-            let des = (request.deserialize)(&body)?;
-
-            Ok(des)
+            Ok((request.to_string)(response)?)
         }
     }
 }
